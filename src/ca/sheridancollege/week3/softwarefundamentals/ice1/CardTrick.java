90dba558f4851dbf122062211df26f5534c38b2b
bollings <50709723+bollings@users.noreply.github.com>
2019-05-23 13:20:52 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author dancye 
+ * Modifier Stuart Bollinger
  */
 public class CardTrick {
     
@@ -22,24 +23,36 @@ public class CardTrick {
         int valueGuess = input.nextInt();
         System.out.println("Now guess the card suit: ");
         String suitGuess = input.next();
-        boolean gameOver = false;
+        boolean magicWinner = false;
+        boolean luckyWinner = false;
+        Card luckyCard = new Card(7,"Clubs");
     
         for (int i=0; i<magicHand.length; i++){
             Card c = new Card();
             c.setValue((int)(Math.random() * 13 + 1));
             c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            System.out.println(c.getValue() + " of " + c.getSuit());
             if ((c.getValue() == valueGuess)&&(c.getSuit().equals(suitGuess))){
-                gameOver = true;
+                magicWinner = true;
                 break;
-            }else{
-               System.out.println(c.getValue() + " of " + c.getSuit());
-               }
             }
-            
-        if (gameOver == false){
-            System.out.println("Game over, no match, better luck next time!");
-        }else if (gameOver){
-            System.out.println("Congatulations, you picked the magic card!");
-        }  
+            if((c.getValue() == luckyCard.getValue()) && (c.getSuit().equals(luckyCard.getSuit()))){
+                luckyWinner = true;
+            }
+        }
+       
+        if (magicWinner){
+             System.out.println("Congatulations, you picked the magic card!");
+        }
+        else if (magicWinner == false){
+            System.out.println("Sorry no match, don't worry you might win with the lucky card!");
+            System.out.println("The lucky card is: " + luckyCard.getCard());
+            if (luckyWinner){
+                System.out.println("Lucky card is a match, you win!");
+            }
+            else {
+                System.out.println("No luck...");
+            }
+        }
     }   
 }
