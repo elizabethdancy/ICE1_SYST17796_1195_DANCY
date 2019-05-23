239cae8c943233c78066a4a0710fccf71def9410
Dackka <stevengeorgejamescarleton@gmail.com>
2019-05-23 11:02:37 -0400

    Fixed so that it runs, oops

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
@@ -13,6 +8,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier
  * hello.
+ * Modifier Steven Carleton
  *
  * @author lamsteve
  */
