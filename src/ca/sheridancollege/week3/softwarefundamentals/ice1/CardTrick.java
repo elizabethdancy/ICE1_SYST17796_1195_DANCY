fa2d3d7f26c64d84e4c90dd53db0d733e1e04bc0
AvaxiaohongDeng <xiaohongdeng1976@gmail.com>
2019-05-23 19:16:41 -0400

    replacing the code with user input to create a code with luckyCard Object in CardTrick class.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -8,8 +8,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * and then asks the user to pick a card and searches the array of 
+ * cards for the match to the user's card. To be used as starting code 
+ * in ICE 1
  * @author Xiaohong Deng Student ID:991517517
  */
 public class CardTrick {
@@ -23,20 +24,18 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             c.setValue((int)(Math.random()*13+1));
-             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+             //c.setSuit(insert call to random number between 0-3 here])
             c.setSuit( NumberToSuit(Math.random()*4)); 
             magicHand[i]=c;
-        }
-        //add a (hard-coded) Card Object called luckyCard with a the number 4 and suit "Hearts"
-        Card luckyCard=new Card();
-        luckyCard.setValue(4);
-        luckyCard.setSuit("Hearts");
-        
+        }  
+    
+    //replacing the code with user input to create a code with luckyCard Object    
+    /*    
         //insert code to ask the user for Card value and suit, create their card
+        boolean found=false;
         Scanner input=new Scanner(System.in);        
         System.out.println("Enter a integer");        
-        int UserCardValue=input.nextInt();
-        
+        int UserCardValue=input.nextInt();        
         System.out.println("Enter a suit(Hearts, or Clubs, or Diamond, or Spades");
         String UserCardSuit=input.next();
         
@@ -44,13 +43,34 @@ public class CardTrick {
          // and search magicHand here
         for(int i=0; i<magicHand.length; i++){
             //Then report the result here
-            if(magicHand[i].getSuit().equals(UserCardSuit) && magicHand[i].getValue()==UserCardValue)
-                System.out.println("Guess is right!");  
-            else
-                System.out.println("Guess is not right!"); 
-        }           
-       
+            if(magicHand[i].getSuit().equals(UserCardSuit) && 
+                    magicHand[i].getValue()==UserCardValue){
+                System.out.println("Congradulation! You Win"); 
+                found=true;
+                break;
+            }
+        }
+            if(!found) System.out.println("Sorry! You Lose");        
+    */
         
+        //add a Card Object called luckyCard with number 4 and suit "Hearts"
+        Card luckyCard=new Card();
+        boolean found=false;
+        luckyCard.setValue(4);
+        luckyCard.setSuit("Hearts");
+        
+        // and search magicHand here
+        for(int i=0; i<magicHand.length; i++){
+            //Then report the result here
+            if(magicHand[i].getSuit().equals(luckyCard.getSuit()) && 
+                    magicHand[i].getValue()==luckyCard.getValue()) {
+                System.out.println("Congradulation! You Win"); 
+                found=true;
+                break;
+            }         
+        }
+        if (!found) System.out.println("Sorry! You Lose");      
+            
     }
     public static String NumberToSuit(double number) {
       String suit;
