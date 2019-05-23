237694dc21847637080f5ddfe54a675ec642e1a2
edwarddegrano <degrano@sheridancollege.ca>
2019-05-23 13:01:45 -0400

    committing to branch, edwarddegrano

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,14 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier edward degrano 991411274, 2019-05-23
  */
 public class CardTrick {
     
@@ -21,12 +24,37 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            int rValue = ((int) (Math.random() * 13) + 1);
+            c.setValue(rValue);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int rSuit = ((int) (Math.random() * 3));
+            c.setSuit(Card.SUITS[rSuit]);
+            magicHand[i] = c;
+        }
+        
+        
+        for(int a = 0; a < 7; a++) {
+            System.out.println(magicHand[a].getSuit());
+            System.out.println(magicHand[a].getValue());
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter a Card value and suit: ");
+        Scanner in = new Scanner(System.in);
+        int newValue = in.nextInt();    
+        String newSuit = in.next();
+        
         // and search magicHand here
         //Then report the result here
+        for(int j = 0; j < magicHand.length; j++) {
+            if (magicHand[j].equals(newValue) && newSuit.equals(magicHand[j])){
+                System.out.println("You won!");
+                break;
+            }
+            else {
+                System.out.println("You lost!");
+                break;
+            }
+        }
     }
-    
 }
