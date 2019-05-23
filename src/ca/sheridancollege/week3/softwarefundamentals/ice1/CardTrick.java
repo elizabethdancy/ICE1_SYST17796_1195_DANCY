e859e2544651b20d91ce315dcbfb2b44ce596c57
Muaz6 <Muaz6@GL703GE>
2019-05-23 11:37:12 -0400

    ICE1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,17 +18,48 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Scanner sc = new Scanner(System.in);
+        
+        
+         int random = 0;
+         int random2 = 0; 
+         
+         System.out.println("Enter Card Value");
+         int input = sc.nextInt();
+         System.out.println("Enter Suit");
+         String input2 = sc.next();
+         Card c = new Card();
+         
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+            
+            
+            random = (int)Math.random() * (13) + 1;
+            random2 = (int)Math.random() * 4;
+            c.setValue(random);
+            c.setSuit(Card.SUITS[random2]);
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
         }
+        
+        if(c.getValue() == input && c.getSuit().equals(input2)){
+            System.out.println("Match");
+        }
+        
+        else{
+            System.out.println("Not a match");
+        }
+        
+    }
+}
+       
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
     
-}
+    
+
