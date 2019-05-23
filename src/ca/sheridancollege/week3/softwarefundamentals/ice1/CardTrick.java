e5dfa3f59df280e94ed87d5559f35292889e0e4f
TravisWright <travis.h5720@gmail.com>
2019-05-23 12:11:56 -0400

    New repo for Students Code.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Arrays;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -13,20 +14,40 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
+    public static void main(String[] args, int i)
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (Card magicHand1 : magicHand) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+            
+            c.setValue((int) (Math.random() * 13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            
+            magicHand[i] = c;
         }
-        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        Scanner inp = new Scanner(System.in);
+        
+        System.out.println("Enter a card number between 1 & 13");
+        int urChoice = inp.nextInt();
+        
+        System.out.println("Enter a Suit For that Card:");
+        int urSuit = inp.nextInt();
+        
+        if(Arrays.asList(magicHand).contains(urChoice))
+            if (Arrays.asList(magicHand).contains(urSuit))
+                System.out.println("Correct Choice!!!");
+            
+            else {
+                System.out.println("Wrong Choice");
+            }
+            
+            
     }
     
 }
