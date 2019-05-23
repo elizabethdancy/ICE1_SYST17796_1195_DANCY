2d91989d5d1d124df90cff97c542f4afc86cf0b1
andre <mceachan@sheridancollege.ca>
2019-05-23 12:47:59 -0400

    pushing initial unedited file for in class assignment

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.*;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -19,14 +21,28 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
+            
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(((int)Math.random()*13)+1);
+            //System.out.println(c);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[((int)Math.random()*4)]);
+            magicHand[i] = c;
         }
-        
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a card value:");
+        int userCard = scan.nextInt();
+        System.out.println("Enter a card suit");
+        String userSuit = scan.next();
+        
         //Then report the result here
+        if(Arrays.asList(magicHand).contains(userCard)){
+            System.out.print("This is your card!");
+        }else
+            System.out.println("Your card is not available");
+        
+        System.out.println(Arrays.toString(magicHand));
     }
-    
 }
