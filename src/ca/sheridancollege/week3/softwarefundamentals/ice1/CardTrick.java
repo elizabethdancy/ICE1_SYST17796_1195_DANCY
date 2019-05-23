463458bc131b61e29b2d32a14a2f61b7fd24437c
stuar <stuar@DESKTOP-8DSATLV>
2019-05-23 11:03:34 -0400

    FirstVersion Shapes

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,7 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -13,20 +14,35 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        
         Card[] magicHand = new Card[7];
+        System.out.println("Card Game - Pick a Card, Any Card!\nFirst, guess the card value: (1 for Ace, 13 for K) ");
+        Scanner input = new Scanner(System.in);
+        int valueGuess = input.nextInt();
+        System.out.println("Now guess the card suit: ");
+        String suitGuess = input.next();
+        boolean gameOver = false;
+        int counter = 0;
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+        do 
+            {for (int i=0; i<magicHand.length; i++){
+                Card c = new Card();
+                c.setValue((int)(Math.random() * 50 + 1));
+                c.setSuit(Card.SUITS[(int)(Math.random() * 3) + 1]);
+                if ((c.getValue() == valueGuess)&&(c.getSuit().equals(suitGuess))){
+                    gameOver = true;
+                }else{
+                    System.out.println(c.getValue() + " of " + c.getSuit());
+                    counter++;
+                }                    
+            }
+        }while(counter <= 7);
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
+        if (gameOver == false){
+            System.out.println("Game over, no match, better luck next time!");
+        }else if (gameOver){
+            System.out.println("Congatulations, you picked the magic card!");
+        }  
+    }   
 }
