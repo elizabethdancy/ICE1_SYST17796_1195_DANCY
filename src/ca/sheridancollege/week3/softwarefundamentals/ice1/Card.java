d2be99521173647d9601c4fcdf505f30d6cdbba3
danielminami <danielminami@hotmail.com>
2019-05-23 11:16:58 -0400

    Created CardTrick to get user Input and compare with the 7 cards hand

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -7,13 +7,17 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
+ * 
+ * 
  * @author dancye
+ * @author Daniel Minami
  */
+
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
@@ -45,9 +49,26 @@ public class Card {
      * @param value the value to set
      */
     public void setValue(int value) {
-        this.value = value;
+        
+        if (value >= 1 && value <= 13) {
+            this.value = value;
+        } else {
+            throw new IllegalArgumentException("Error: values must be between"
+                    + " 1 and 13");
+        }
     }
-   
-   
+    
+   @Override
+   public boolean equals(Object obj) {
+       
+       Card card = (Card)obj;
+       if (this.getValue() == card.getValue() &&
+           this.getSuit() == card.getSuit()) {
+           return true;
+       } else {
+           return false;
+       }
+       
+   }
     
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,28 +5,97 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ * 
  * @author dancye
+ * @author Daniel Minami
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Scanner in;
+        Card userCard = new Card();
+        int userValue = 0;
+        boolean validEntry = false;
+        String winnerMsg = "winnerMsg = \"You lose :-(\"";
+        
+        
+        /*
+            Modified by Daniel Minami
+            Student Number: 991548108
+        */
+        
         
         for (int i=0; i<magicHand.length; i++)
-        {
+        {    
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random() * 13) + 1);
+            c.setSuit(Card.SUITS[(int)(Math.random() * 3)]);
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        do {
+            try {
+                System.out.print("Enter the Card value (1-13): ");
+                in = new Scanner(System.in);
+                userValue = in.nextInt();
+                userCard.setValue(userValue);
+                validEntry = true;
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!validEntry);
+        validEntry = false;
+        
+        do {
+            try {
+                System.out.println("The Card suit where: "
+                        + "\n0 - Hearts \n1 - Diamonds\n2 - Spades\n3 - Clubs");
+                in = new Scanner(System.in);
+                userValue = in.nextInt();
+                if (validateSuit(userValue)) {
+                    userCard.setSuit(userCard.SUITS[userValue]);
+                    validEntry = true;
+                    System.out.println();
+                } else { 
+                    System.out.println("Error: Please enter a # between 0-3");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!validEntry);
+        
+        System.out.println(userCard.getValue());
+        System.out.println(userCard.getSuit());
+        
+        for (int i = 0; i < magicHand.length; i++) {
+            if (userCard.equals(magicHand[i])) {
+                winnerMsg = "You win!";
+            }
+        }
+        
+        System.out.println(winnerMsg);
+    }
+        
+    public static boolean validateSuit(int suitIndex) {
+        
+        if (suitIndex >= 0 && suitIndex <= 3){
+            return true;
+        } else {
+            return false;
+        }
+        
     }
     
 }
